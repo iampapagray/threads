1a6026804b3a1e2aec9ddad1bc1705ce3c39dc0b
Bryan Graham <iampapagray@gmail.com>
2023-08-05 00:29:16 +0000

    Auth and Content Layout

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,23 @@
+import '../globals.css'
+import { ClerkProvider } from '@clerk/nextjs'
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+    title: 'Threads',
+    description: 'NextJs 13 Meta Threads App',
+}
+
+export default function RootLayout({ children } : {children: React.ReactNode}) {
+    return (
+        <ClerkProvider>
+            <html lang='en'>
+                <body className={`${inter.className} bg-dark-1`}>
+                    {children}
+                </body>
+            </html>
+        </ClerkProvider>
+    )
+}
